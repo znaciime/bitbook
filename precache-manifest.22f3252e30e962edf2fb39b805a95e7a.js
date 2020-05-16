6e2332dbd5f60d6e51c726e9b2fac8ff83abd53e
znaciime <zubzubyo@gmail.com>
2020-05-16 16:11:35 +0200

    Updates

---
+++ precache-manifest.22f3252e30e962edf2fb39b805a95e7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1701565b181b386bf5ee",
+    "url": "/bitbooka/static/css/main.45ea5b12.chunk.css"
+  },
+  {
+    "revision": "1701565b181b386bf5ee",
+    "url": "/bitbooka/static/js/main.28916c29.chunk.js"
+  },
+  {
+    "revision": "61f97f2486ff68dd7eb1",
+    "url": "/bitbooka/static/js/runtime~main.656de0a7.js"
+  },
+  {
+    "revision": "9d556e7acafa6a9510b9",
+    "url": "/bitbooka/static/js/2.ffbcccd2.chunk.js"
+  },
+  {
+    "revision": "57803829a28d3c5dd4399717c85aa737",
+    "url": "/bitbooka/static/media/not_found.57803829.png"
+  },
+  {
+    "revision": "51faacfcbab699f8295fe1135ef2e0d4",
+    "url": "/bitbooka/index.html"
+  }
+];
